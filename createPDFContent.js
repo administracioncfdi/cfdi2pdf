233e56d7ac548698d9fbd1fc8dc4ae4531c35719
unknown <manuel@quad-tree.com>
2018-01-30 11:24:54 -0600

    Added file for function that generates pdf content

---
+++ createPDFContent.js
@@ -0,0 +1,7 @@
+/**
+* Receives a json and returns a pdf content object for pdfmake
+* @param {Object} json result json from using parseData function
+*/
+export var createPDF = function(json){
+  return
+}
