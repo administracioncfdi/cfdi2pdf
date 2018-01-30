7d5a3c4a1d3e3c3d6b1311c76b25ca796a996a27
unknown <manuel@quad-tree.com>
2018-01-30 11:25:36 -0600

    Added both pdf content example and xml example to test functions

---
+++ pdfmakeExample.js
@@ -0,0 +1,19 @@
+export var pdfmakeExample = {
+  content: [
+    { text: 'This is a header', style: 'header' },
+    'No styling here, this is a standard paragraph',
+    { text: 'Another text', style: 'anotherStyle' },
+    { text: 'Multiple styles applied', style: [ 'header', 'anotherStyle' ] }
+  ],
+
+  styles: {
+    header: {
+      fontSize: 22,
+      bold: true
+    },
+    anotherStyle: {
+      italics: true,
+      alignment: 'right'
+    }
+  }
+};
