db87612b2132c078925ac8775a8295fde09d2bd8
unknown <manuel@quad-tree.com>
2018-02-08 14:56:16 -0600

    Modified createPDFServer function

--- createPDFServer.js
+++ createPDFServer.js
@@ -18,23 +18,27 @@ var PdfPrinter = require('pdfmake/src/printer');
 * @param {Object} font fonts to be used for pdfMake
 * @param {Object} response response sent from the server to the client
 */
-var createPDFServer = function(xml, fonts, response){
-  xml = xmlExample //EXAMPLE
-  return parseString(xml, function(err, res){
-    if(res){
-      var json = parseData(res)
-      console.log(json)
-      var content = createPDFContent(json)
-      console.log(content)
-      var printer = new PdfPrinter(fonts);
-      var doc = printer.createPdfKitDocument(content);
-      //var doc = printer.createPdfKitDocument(pdfmakeExample) //EXAMPLE
-      doc.pipe(response)
-      doc.end()
-    }else{
-      throw err
-    }
-  })
+var createPDFServer = function(xml, response, options){
+  if(options.fonts){
+    xml = xmlExample //EXAMPLE
+    return parseString(xml, function(err, res){
+      if(res){
+        var json = parseData(res)
+        console.log(json)
+        var content = createPDFContent(json, options.image)
+        console.log(content)
+        var printer = new PdfPrinter(options.fonts);
+        var doc = printer.createPdfKitDocument(content);
+        //var doc = printer.createPdfKitDocument(pdfmakeExample) //EXAMPLE
+        doc.pipe(response)
+        doc.end()
+      }else{
+        throw err
+      }
+    })
+  }else{
+    throw "You need to define the fonts to be used in the options"
+  }
 }
 
 module.exports = createPDFServer;
